e36f2c75b0efe02ed9a771114d09c48066e78c4e
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2025-01-13 20:47:27 +0400

    Delete level28/classwork/cw2.py

--- level28/classwork/cw2.py
+++
@@ -1,6 +0,0 @@
-#2შექმენით სია, რომელიც შეიცავს 5 სხვადასხვა ხილის სახელს. შეცვალეთ მესამე ხილის სახელი და გამოიტანეთ განახლებული სია.
-
-list = ["marwyvi", "bali", "alubali", "msxali", "atami"]
-list.pop(2)
-list.append("alubali")
-print(list)
