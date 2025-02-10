ebd6800320e5d180de11971e4cabcb6b089cb415
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2025-02-10 19:44:20 +0400

    second commit

---
+++ level28/classwork/cw1.py
@@ -0,0 +1,10 @@
+#შექმენით სია, რომელიც შეიცავს 6 სხვადასხვა ცხოველის სახელს. წაშალეთ მეორე და მეოთხე ცხოველის სახელი და გამოიტანეთ განახლებული სია.
+
+animals = ["maimuni", "datvi", "zebra", "mgeli", "lomi", "vefxvi"]
+
+
+animals.remove("datvi")
+
+animals.remove("mgeli")
+
+print(animals)
---
+++ level28/classwork/cw2.py
@@ -0,0 +1,9 @@
+#შექმენით სია, რომელიც შეიცავს 5 სხვადასხვა ხილის სახელს. შეცვალეთ მესამე ხილის სახელი და გამოიტანეთ განახლებული სია.
+
+
+fruits = ["bali", "alubali", "vashli", "msxali", "marwyvi"]
+
+fruits.pop(2)
+fruits.append("vashli")
+
+print(fruits)
