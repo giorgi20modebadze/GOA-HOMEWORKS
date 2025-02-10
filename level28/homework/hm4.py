955d6fc5f0c52310535fcad991887bfe3ea511bd
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2025-02-10 19:30:28 +0400

    second commit

---
+++ level28/homework/hm4.py
@@ -0,0 +1,8 @@
+#შექმენით სია, რომელიც შეიცავს 8 სხვადასხვა ქალაქის სახელებს. წაშალეთ მესამე და მეხუთე ქალაქის სახელი და გამოიტანეთ განახლებული სია.
+
+city = ["batumi", "qutaisi", "tbilisi", "zestafoni", "rustavi", "gori", "telavi", "zugdidi"]
+
+city.pop(2)
+city.pop(4)
+
+print(city)
