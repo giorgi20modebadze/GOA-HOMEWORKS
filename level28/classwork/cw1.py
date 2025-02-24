09549b123ff7685a1f3a3af1e2eb9a0b3d68afa8
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2025-02-24 20:51:10 +0400

    second commit

---
+++ level28/classwork/cw1.py
@@ -0,0 +1,8 @@
+#შექმენით სია, რომელიც შეიცავს 6 სხვადასხვა ცხოველის სახელს. წაშალეთ მეორე და მეოთხე ცხოველის სახელი და გამოიტანეთ განახლებული სია.
+
+animals = ["maimuni", "datvi", "zebra", "mgeli", "lomi", "leopardi"]
+
+animals.pop(1)
+animals.pop(3)
+
+print(animals)
