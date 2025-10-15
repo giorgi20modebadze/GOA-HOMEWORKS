e23bdcbecdf52e8ff02b68a4aeb6968417354633
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2025-10-15 22:08:01 +0400

    second commit

---
+++ level102/classwork/cw2.js
@@ -0,0 +1,16 @@
+//შექმენი ფუნქცია,შემდეგ შექმენით 3 ცვლადი,1 ში შეინახეთ სახელი მეორეში ასაკი და მესამეში საცხოვრებელია დგილი,თქვენი დავალებაა რომ ფუნქციამ კონსოლში გამოიტანოს hello , my name is {name} and i am {age} years old, i live in {city} , გამოიძახე ფუნქცია 5 ჯერ 
+
+
+function func() {
+    let name = "giorgi";
+    let age = 21;
+    let city = "tbilisi";
+
+    console.log("Hello, my name is " + name + " and I am " + age + " years old, I live in " + city);
+}
+
+func();
+func();
+func();
+func();
+func();
