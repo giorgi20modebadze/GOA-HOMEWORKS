f142d01a2035bdf802d3c77cdfe883b89442f760
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2025-10-21 19:34:05 +0400

    Delete level097 directory

--- level097/homework/hw2.js
+++
@@ -1,4 +0,0 @@
-//2)კომენტარის სახით ჩამოწერეთ რამდენი გზით შეგვიძლია რომ შევქმნათ ცვვლადები და ჩამოწერე რა განსხვავებაა მათ შორის.
-
-// cvladebis sheqmnis ramdenime gza arsebobs let, const, var
-//mtavari gansxvaveba isaa rom var - is da let - is cvladis icvleba xolo const ar icvleba
--- level097/homework/hw7.js
+++
@@ -1,7 +0,0 @@
-//შექმენით 5 ცვლადი camelCase ის გამოყენებით და შეინახეთ 5 ივე ცვლადში number ტიპის მონაცემი
-
-let userAge = 21
-let productPrice = 459.99
-let totalMoney= 9000
-let itemCount = 12
-let temperatureCelsius = 20
