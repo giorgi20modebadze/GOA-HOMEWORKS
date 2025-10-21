5f976461daea6c1b201670211b7de191be309c85
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2025-10-21 20:20:18 +0400

    Delete level028/classwork directory

--- level028/classwork/cw1.py
+++
@@ -1,8 +0,0 @@
-#შექმენით სია, რომელიც შეიცავს 6 სხვადასხვა ცხოველის სახელს. წაშალეთ მეორე და მეოთხე ცხოველის სახელი და გამოიტანეთ განახლებული სია.
-
-animals = ["maimuni", "datvi", "zebra", "mgeli", "lomi", "leopardi"]
-
-animals.pop(1)
-animals.pop(3)
-
-print(animals)
--- level028/classwork/cw2.py
+++
@@ -1,8 +0,0 @@
-#შექმენით სია, რომელიც შეიცავს 5 სხვადასხვა ხილის სახელს. შეცვალეთ მესამე ხილის სახელი და გამოიტანეთ განახლებული სია.
-
-fruits = ["vashli", "msxali", "atami", "bali", "alubali"]
-
-fruits.pop(2)
-fruits.append("atami")
-
-print(fruits)
