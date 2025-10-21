16951c6b976c60f3284fd05ddd0a5ed4a3da5c2a
giorgi20modebadze <modebadzegiorgi341@gmail.com>
2025-10-21 19:59:21 +0400

    Delete level061 directory

--- level061/homework/hw1.py
+++
@@ -1,7 +0,0 @@
-#  ახსენით რა არის keyframes, animations  და რისთვის ვიყენებთ. განმარტეთ თითოეული ქვე-კუთვნილება: animation-name, animation-duration, animation-delay, animation-iteration-count, animation-direction, animation-timing-function
-
-
-
-# Keyframes და Animations არიან ის მექანიზმები, რომლებიც საშუალებას გვაძლევს შექმნათ რთული ანიმაციები CSS-ში, რომლებსაც სძენს ვიზუალურ ეფექტებს სხვადასხვა ელემენტებს.
-#Keyframes არიან სტრუქტურული წერტილები ანიმაციაში, რომლებიც განსაზღვრავენ ელემენტის მდგომარეობას კონკრეტულ დროს.                                           
-#animation-name, animation-duration, animation-delay, animation-iteration-count, animation-direction და animation-timing-function ყველაფერს მართავენ ანიმაციის შესახებ და ასრულებენ მის მართვადობას, გართულებას და კონტროლს.
